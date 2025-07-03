8893228d623c7b9a2dfa9189399d1ba69b945de5
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-03 19:57:56 +0200

    Add components views

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import AddScreen from './components/pages/AddScreen/AddScreen';
 import EditScreen from './components/pages/EditScreen/EditScreen';
 import About from './components/pages/About/About';
 import NoPage from './components/pages/NoPage/NoPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/post/:id' element={<Post />} />
-      <Route path='/post/:add' element={<AddScreen />} />
-      <Route path='/post/edit/:id' element={<EditScreen />} />
-      <Route path='/about' element={<About />} />
-      <Route path='*' element={<NoPage />} />
-    </Routes>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/post/:id' element={<Post />} />
+        <Route path='/post/:add' element={<AddScreen />} />
+        <Route path='/post/edit/:id' element={<EditScreen />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NoPage />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
---
+++ src/components/views/Header/Header.jsx
@@ -0,0 +1,5 @@
+import NavBar from '../NavBar/NavBar';
+
+export default function Header() {
+  return <NavBar />;
+}
