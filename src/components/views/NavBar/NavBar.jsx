4d0c4184ea9aa105bc476caeacb9e999d306e412
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-03 21:23:10 +0200

    Add structure to component NavBar

--- src/components/views/NavBar/NavBar.jsx
+++ src/components/views/NavBar/NavBar.jsx
@@ -1,3 +1,18 @@
+import { Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className='d-flex justify-content-between align-items-center px-4 py-3 mt-2 bg-primary text-white rounded'>
+        <h3>Blog.app</h3>
+      <Nav>
+        <Nav.Link as={NavLink} to={'/'} className='text-white'>
+          Home
+        </Nav.Link>
+        <Nav.Link as={NavLink} to={'/about'} className='text-white'>
+          About
+        </Nav.Link>
+      </Nav>
+    </div>
+  );
 }
