cc2f13144657e489010bf58b0665549a70829c40
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-04 15:10:52 +0200

    Add structure component Footer

--- src/components/views/Footer/Footer.jsx
+++ src/components/views/Footer/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className='text-center py-3 bg-light ng-oapcity-50'>
+      <p className='text-secondary'>Copyright &copy; BlogApp 2025</p>
+    </div>
+  );
 }
